3c11a08b4b6cdf2b7ce3d631c5dda5121d74f3b0
AND0MASTER <viktormalovany@seznam.cz>
2025-07-27 13:58:03 +0200

    player script changes

--- Script/player.gd
+++ Script/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -150.0
+const SIZE =2.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +13,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY * SIZE
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
