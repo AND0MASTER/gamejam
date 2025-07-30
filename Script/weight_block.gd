e70cea5aed7e84340f3c8d68c9bfc92b9c2bbbc2
AND0MASTER <viktormalovany@seznam.cz>
2025-07-30 19:02:56 +0200

    weight block timer

--- Script/weight_block.gd
+++ Script/weight_block.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+@onready var timer: Timer = $Timer
 
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if AutoLoadedScript.SIZE == 1:
-		queue_free()
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	queue_free()
