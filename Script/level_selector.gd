d956ed28c36314b31a02d73fa343d63c7a362ca5
HeadlessHatter <pixelmed@seznam.cz>
2025-07-31 12:19:41 +0200

    kill zone and level selector script change

--- Script/level_selector.gd
+++ Script/level_selector.gd
@@ -1,8 +1,34 @@
 extends Control
 
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
 
-func _on_return_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_3.tscn")
+
+func _on_level_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_4.tscn")
+
+func _on_level_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_5.tscn")
+
+func _on_level_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_6.tscn")
+
+func _on_level_7_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_7.tscn")
+
+func _on_level_8_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_8.tscn")
+
+func _on_level_9_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_9.tscn")
+
+func _on_level_10_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_10.tscn")
