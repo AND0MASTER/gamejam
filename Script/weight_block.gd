ed71c34535fb637c5405614300912e224ff65c39
HeadlessHatter <pixelmed@seznam.cz>
2025-08-01 18:59:10 +0200

    weight block now cracks in any size

--- Script/weight_block.gd
+++ Script/weight_block.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @onready var timer: Timer = $Timer
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
+func _on_ready() -> void:
+	animated_sprite_2d.play("Default")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if AutoLoadedScript.SIZE == 1:
+		animated_sprite_2d.play("cracked")
 		timer.start()
 
 
